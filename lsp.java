8b7118028383e136fab6c419dfbc1e671d4523dd
flamez57 <flamez57@qq.com>
2023-11-15 17:19:46 +0800

    lsp

--- lsp.java
+++ lsp.java
@@ -9,7 +9,46 @@ import java.util.List;
 
 public class lsp {
     public static void main(String[] args) {
-        File folder = new File("1974");
+        if (args.length > 0) {
+            System.out.println("get param:"+args[0]);
+        }
+        
+        System.out.println("param length:"+args.length);
+
+        FolderUtil f = new lsp.FolderUtil(args[0]);
+        f.fileCount();
+        List<PicCount> picCounts = f.getPicCountList();
+        System.out.println(picCounts);
+        //fileCount(args[0]);
+        /*System.out.println("lsp");
+
+        List<String> files = getAllFiles("1973");
+        for (String file : files) {
+            System.out.println(file);
+        }*/
+        //System.out.println(files);
+
+    
+        
+        //File oldFile = new File("OldFileName.txt");
+        
+        //File newFile = new File("NewFileName.txt");
+        
+        //boolean result = oldFile.renameTo(newFile);
+        
+        /*if (result) {
+            System.out.println("ok");
+        } else {
+            System.out.println("no");
+        }*/
+        
+    }
+
+    /*
+     * 
+     */
+    public static void formatePicName(String folderPath) {
+        File folder = new File(folderPath);
         File[] files = folder.listFiles();
         int i = 0;
         if (files != null) {
@@ -39,28 +78,75 @@ public class lsp {
                 }
             }
         }
-        /*System.out.println("lsp");
+    }
 
-        List<String> files = getAllFiles("1973");
-        for (String file : files) {
-            System.out.println(file);
-        }*/
-        //System.out.println(files);
+    public static int fileCount(String folderPath) {
+        List<PicCount> picCountList = new ArrayList<>();
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+        int num = 0;
+        if (files != null) {
+            for (File file : files) {
+                //System.out.println(file.getName());                
+                //System.out.println(file.getParentFile());
+                //System.out.println("======");
 
-    
-        
-        //File oldFile = new File("OldFileName.txt");
-        
-        //File newFile = new File("NewFileName.txt");
-        
-        //boolean result = oldFile.renameTo(newFile);
-        
-        /*if (result) {
-            System.out.println("ok");
-        } else {
-            System.out.println("no");
-        }*/
-        
+                if (file.isFile()) {
+                    num++;
+                } else if (file.isDirectory()) {
+                    picCountList.add(new PicCount(file.getName(), fileCount(file.getAbsolutePath())));
+                    System.out.println(file.getName() + "===" + fileCount(file.getAbsolutePath()));
+                }
+            }
+        }
+        return num;
+    }
+
+    class FolderUtil {
+        private List<PicCount> picCountList;
+        private String folderPath;
+
+        public FolderUtil(String folderPath) {
+            this.folderPath = folderPath;
+        }
+
+        public int fileCount() {
+            File folder = new File(folderPath);
+            File[] files = folder.listFiles();
+            int num = 0;
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        num++;
+                    } else if (file.isDirectory()) {
+                        picCountList.add(new PicCount(file.getName(), this.fileCount(file.getAbsolutePath())));
+                    }
+                }
+            }
+            return num;
+        }
+
+        public List<PicCount> getPicCountList() {
+            return this.picCountList;
+        }
+    }
+
+    /*
+     * Number of files in a folder data model
+     */
+    class PicCount {
+        String name;
+        int qty;
+        public PicCount(String name, int qty) {
+            this.name = name;
+            this.qty = qty;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public void setQty(int qty) {
+            this.qty = qty;
+        }
     }
 
     public static List<String> getAllFiles(String folderPath) {
