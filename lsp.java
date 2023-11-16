e4593cf1561177f9f82b9d697c11dad50642f4a7
flamez57 <flamez57@qq.com>
2023-11-16 09:19:33 +0800

    lspok

--- lsp.java
+++ lsp.java
@@ -10,38 +10,25 @@ import java.util.List;
 public class lsp {
     public static void main(String[] args) {
         if (args.length > 0) {
-            System.out.println("get param:"+args[0]);
+            // get param
+            String folder = args[0]; 
+
+            // assign folder
+            FolderUtil f = new FolderUtil(folder);
+
+            if (args.length > 1) {
+                f.formatePicName();
+            }
+
+            // statistics files num
+            f.fileCount();
+
+            // point files num
+            List<PicCount> picCounts = f.getPicCountList();
+            System.out.println("{\"name\":\""+folder+"\",\"list\":"+picCounts+"}");
         }
         
         System.out.println("param length:"+args.length);
-
-        FolderUtil f = new FolderUtil(args[0]);
-
-        List<PicCount> picCounts = f.getPicCountList();
-        System.out.println(picCounts);
-        //fileCount(args[0]);
-        /*System.out.println("lsp");
-
-        List<String> files = getAllFiles("1973");
-        for (String file : files) {
-            System.out.println(file);
-        }*/
-        //System.out.println(files);
-
-    
-        
-        //File oldFile = new File("OldFileName.txt");
-        
-        //File newFile = new File("NewFileName.txt");
-        
-        //boolean result = oldFile.renameTo(newFile);
-        
-        /*if (result) {
-            System.out.println("ok");
-        } else {
-            System.out.println("no");
-        }*/
-        
     }
 
     /*
@@ -80,31 +67,6 @@ public class lsp {
         }
     }
 
-    public static int fileCount(String folderPath) {
-        List<PicCount> picCountList = new ArrayList<>();
-        File folder = new File(folderPath);
-        File[] files = folder.listFiles();
-        int num = 0;
-        if (files != null) {
-            for (File file : files) {
-                //System.out.println(file.getName());                
-                //System.out.println(file.getParentFile());
-                //System.out.println("======");
-
-                if (file.isFile()) {
-                    num++;
-                } else if (file.isDirectory()) {
-                    picCountList.add(new PicCount(file.getName(), fileCount(file.getAbsolutePath())));
-                    System.out.println(file.getName() + "===" + fileCount(file.getAbsolutePath()));
-                }
-            }
-        }
-        return num;
-    }
-
-
-
-
     public static List<String> getAllFiles(String folderPath) {
         List<String> fileList = new ArrayList<>();
         File folder = new File(folderPath);
@@ -130,13 +92,48 @@ public class lsp {
 class FolderUtil {
     private List<PicCount> picCountList;
     private String folderPath;
-
     public FolderUtil(String folderPath) {
         this.folderPath = folderPath;
+    }
+
+    public void formatePicName() {
+        File folder = new File(this.folderPath);
+        File[] files = folder.listFiles();
+        int i = 0;
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println(file.getName() + ", this is a file nothing todo");
+                } else if (file.isDirectory()) {
+                    System.out.println("======");
+                    System.out.println(file.getAbsolutePath());
+                    File _file = new File(file.getAbsolutePath());
+                    File[] _files = _file.listFiles();
+                    if (_files != null) {
+                        i = 0;
+                        for (File _f : _files) {
+                            if (_f.isFile()) {
+                                System.out.println(_f.getName());
+
+                                String _newFileName = _f.getParentFile() + "/" + i +_f.getName().substring(_f.getName().lastIndexOf("."));
+                                i++;
+                                System.out.println(_newFileName);
+                                File _newFile = new File(_newFileName);
+                                _f.renameTo(_newFile);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void fileCount() {
+        this.picCountList = new ArrayList<>();
         fileCount(this.folderPath);
     }
 
-    public int fileCount(String folderPath) {
+    private int fileCount(String folderPath) {
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
         int num = 0;
@@ -145,7 +142,7 @@ class FolderUtil {
                 if (file.isFile()) {
                     num++;
                 } else if (file.isDirectory()) {
-                    picCountList.add(new PicCount(file.getName(), fileCount(file.getAbsolutePath())));
+                    this.picCountList.add(new PicCount(file.getName(), fileCount(file.getAbsolutePath())));
                 }
             }
         }
@@ -172,5 +169,8 @@ class PicCount {
     }
     public void setQty(int qty) {
         this.qty = qty;
+    }
+    public String toString() {
+        return "{\"name\":\""+name+"\",\"qty\":\""+qty+"\"}";
     }
 }
